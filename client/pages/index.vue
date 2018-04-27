7f43cb321e00c12048e0bd11a592dab3462a06da
wxaxiaoyao <765485868@qq.com>
2018-04-27 14:21:19 +0800

    test webhook

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,7 +2,7 @@
   <section class="container">
     <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
     <h1 class="title">
-      Universal Vue.js Application Framework hello world
+      Universal Vue.js Application Framework
     </h1>
     <nuxt-link class="button" to="/about">
       About page
